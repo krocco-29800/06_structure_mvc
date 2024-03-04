2ec538ba93b1875147756d7944f2affc08f5d51b
69VFOMT104-14 <marcls29800@gmail.com>
2024-03-04 10:25:56 +0100

    ADD Folder 11

---
+++ 11_OOP_MVC_RELOADED/index.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+
+use App\Services\Router;
+// On charge la config
+require_once("config.php");
+// On charge l'autoloader
+require_once("autoload.php");
+
+// On détermine quelle page doit être affichée
+// ?page=
+$router = new Router();
+$page = $router->getPage();
+
+// exemple: App\Controllers\HomeController
+$controllerName = "App\Controllers\\".ucfirst($page)."Controller";
+$controller = new $controllerName(); // $controller = new HomeController()
+$controller->index();
